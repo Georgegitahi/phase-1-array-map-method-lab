d9d359bae42e0edaa70493a8ddb4fc3cfa869f60
Georgegitahi <petergeorgembugua724@gmail.com>
2024-04-04 21:19:22 +0300

    lab comlete

--- index.js
+++ index.js
@@ -11,6 +11,28 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
+
+
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((line) => {
+    const tokens = line.split(" ");
+    const capitalizedTokens = tokens.map(
+      (token) => token.charAt(0).toUpperCase() + token.slice(1)
+    );
+    const response = capitalizedTokens.join(" ");
+    return response;
+  });
+};
+
+
+// const titleCased = () => {
+//   return tutorials.map((line)=>{
+//     const tokens = line.split("");
+//     const capitalizedTokens=tokens.map(
+//       (token)=> token.charAt(0).toUpperCase()+token.slice(1)
+//     );
+//     const response = capitalizedTokens.join("");
+//     return response;
+//   });
+// };
